4b1cd148b9f96f2e02c702591840aec9a1fa38e3
NITY70 <osincevanton27@gmail.com>
2024-02-25 11:44:38 +0700

    AlGoodGame(02)

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -32,6 +32,7 @@ public class EnemyAI : MonoBehaviour
     private void Update()
     {
         NoticePlayerUpdate();
+        ChaseUpdate();
         PatrolUpdate();
     }
     private void NoticePlayerUpdate()
@@ -55,15 +56,25 @@ public class EnemyAI : MonoBehaviour
 
     private void PatrolUpdate()
     {
-        if (_navMeshAgent.remainingDistance == 0) 
+        if (!_isPlayerNoticed)
         {
-            PickNewPatrolPoint();
+            if (_navMeshAgent.remainingDistance == 0)
+            {
+                PickNewPatrolPoint();
+            }
         }
-        
     }
     private void PickNewPatrolPoint()
     {
         _navMeshAgent.destination = patrolPoints[Random.Range(0, patrolPoints.Count)].position;
+    }
+
+    private void ChaseUpdate()
+    {
+        if (_isPlayerNoticed)
+        {
+            _navMeshAgent.destination = player.transform.position;
+        }
     }
 
 }
