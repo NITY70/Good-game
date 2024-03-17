4ad408f509afc00532e38ffff7f0d10a439a1822
NITY70 <osincevanton27@gmail.com>
2024-03-17 16:18:56 +0700

    fesfeFSD

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -6,8 +6,14 @@ public class EnemyHealth : MonoBehaviour
 {
     public float value = 100;
 
+    public Progress playerProgress;
+
+    public Explosion explosionPrefab;
+
     public void DealDamage(float damage)
     {
+        playerProgress.AddExperience(damage);
+
         value -= damage;
         if (value <= 0)
         {
